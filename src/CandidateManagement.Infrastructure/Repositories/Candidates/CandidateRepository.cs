7fa13746eb733d9a9b4fae07e2414489348e6d88
Biraj Mainali <birajmainali10@gmail.com>
2025-01-15 22:02:54 +0545

    feat: add candidate repository

---
+++ src/CandidateManagement.Infrastructure/Repositories/Candidates/CandidateRepository.cs
@@ -0,0 +1,11 @@
+﻿using CandidateManagement.Domain.Entities;
+using CandidateManagement.Infrastructure.EntityFrameworkCore.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CandidateManagement.Infrastructure.Repositories.Candidates;
+
+public class CandidateRepository(CandidateManagementDbContext dbContext)
+    : GenericRepository<Candidate>(dbContext), ICandidateRepository
+{
+    public Task<Candidate?> FindByEmailAsync(string email) => dbContext.Candidates.FirstOrDefaultAsync(x => x.Email == email);
+}
---
+++ src/CandidateManagement.Infrastructure/Repositories/Candidates/ICandidateRepository.cs
@@ -0,0 +1,8 @@
+﻿using CandidateManagement.Domain.Entities;
+
+namespace CandidateManagement.Infrastructure.Repositories.Candidates;
+
+public interface ICandidateRepository : IGenericRepository<Candidate>
+{
+    Task<Candidate?> FindByEmailAsync(string email);
+}
