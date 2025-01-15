e4c5f93c7a4e3f47aff3fd7da56dede75a7f9718
Biraj Mainali <birajmainali10@gmail.com>
2025-01-15 22:02:31 +0545

    feat: add generic repository

---
+++ src/CandidateManagement.Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,21 @@
+﻿using CandidateManagement.Domain.Entities;
+using CandidateManagement.Infrastructure.EntityFrameworkCore.Data;
+
+namespace CandidateManagement.Infrastructure.Repositories;
+
+public class GenericRepository<T> where T : class, IEntity
+{
+    private readonly CandidateManagementDbContext _dbContext;
+    protected GenericRepository(CandidateManagementDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    public async Task<T> AddAsync(T t)
+    {
+        await _dbContext.Set<T>().AddAsync(t);
+        return t;
+    }
+    public void Update(T t) => _dbContext.Update(t);
+    
+    public async Task SaveChangesAsync() => await _dbContext.SaveChangesAsync();
+}
---
+++ src/CandidateManagement.Infrastructure/Repositories/IGenericRepository.cs
@@ -0,0 +1,11 @@
+﻿using CandidateManagement.Domain.Entities;
+
+namespace CandidateManagement.Infrastructure.Repositories;
+
+public interface IGenericRepository<T> where T : class, IEntity
+{
+    Task<T> AddAsync(T t);
+    void Update(T t);
+    
+    Task SaveChangesAsync();
+}
